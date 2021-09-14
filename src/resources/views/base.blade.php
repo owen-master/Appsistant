1871f7d9aa810586c9bbaf13112f4f2bb6d9a419
Owen <owen.herbert@hotmail.com>
2021-09-14 17:46:45 +1000

    Add Javascript section

--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -175,4 +175,7 @@
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
         crossorigin="anonymous"></script>
 
+<!-- Javascript Section -->
+@yield('javascript')
+
 </html>
