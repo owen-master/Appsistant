37ec9781929d512ed24374de4ff6cc906476618d
Owen <owen.herbert@hotmail.com>
2021-09-14 17:44:45 +1000

    Add JQuery 3.6.0 from external CDN

--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -169,4 +169,10 @@
         integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
         crossorigin="anonymous"></script>
 
+<!-- JQuery from CDN -->
+<script
+        src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous"></script>
+
 </html>
