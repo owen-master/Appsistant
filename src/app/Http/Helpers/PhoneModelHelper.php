e78ac013a4d6d45f6646b8654c406cd6a7eb0927
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-10-06 14:49:52 +1000

    Implement PhoneModelHelper.

--- src/app/Http/Helpers/PhoneModelHelper.php
+++ src/app/Http/Helpers/PhoneModelHelper.php
@@ -6,13 +6,28 @@ class PhoneModelHelper {
     const DEFAULT_MODE = false;
     const SESSION_MODE = 'SESSION_PHONE_MODE';
 
-    public static function getModel() {
-        return session()->get(self::SESSION_MODE, self::DEFAULT_MODE);
-    }
-
     public static function toggleModel() {
         $mode = session()->get(self::SESSION_MODE, self::DEFAULT_MODE);
         return session()->put(self::SESSION_MODE, !$mode);
     }
+
+    public static function getIphone() {
+        $mode = session()->get(self::SESSION_MODE, self::DEFAULT_MODE);
+
+        if ($mode) {
+            return 'style="display: block"';
+        } else {
+            return 'style="display: none"';
+        }
+    }
+
+    public static function getAndroid() {
+        $mode = session()->get(self::SESSION_MODE, self::DEFAULT_MODE);
+
+        if ($mode) {
+            return 'style="display: none"';
+        } else {
+            return 'style="display: block"';
+        }    }
 
 }
--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -36,6 +36,79 @@
             margin-bottom: 10px;
         }
 
+        .android {
+            width: 21.06em;
+            height: 35.6em;
+            border-radius: 1.2em;
+            background-color: grey;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+
+        .android:before {
+            width: 20.61em;
+            height: 35.15em;
+            border-radius: 1em;
+            background-color: black;
+            position: fixed;
+            display: inline-block;
+            content: '';
+            top: 0.2em;
+            left: 0.2em;
+        }
+
+        .android-details {
+            content: '';
+            width: 5em;
+            height: 0.6em;
+            border-radius: 20%;
+            border: grey solid .2em;
+            position: absolute;
+            top: 1.613em;
+            left: 8.03em;
+        }
+
+        .android-left {
+            width: .188em;
+            height: 2.1250em;
+            top: 6.250em;
+            left: -.188em;
+            position: absolute;
+            background: dimgrey;
+        }
+
+        .android-home {
+            width: 3.25em;
+            height: 1.85em;
+            border-radius: 45%;
+            position: absolute;
+            bottom: 1.125em;
+            right: 50%;
+            -webkit-transform: translateX(50%);
+            transform: translateX(50%);
+            border: .3em solid dimgrey;
+            background-color: black;
+        }
+
+        .android-screen {
+            width: 19.5em;
+            height: 27.938em;
+            position: absolute;
+            top: 3.75em;
+            left: .750em;
+            overflow: hidden;
+        }
+
+        .android-content {
+            position: relative;
+            background: #ffffff;
+            width: 100%;
+            height: 100%;
+        }
+
         .iphone {
             width: 21.06em;
             height: 35.6em;
@@ -147,23 +220,38 @@
 <body>
 
 <form style="position:absolute; right: 1em; top: 1em;">
-    <button type="button" onclick="window.location.href='{{ route('model.get') }}'" name="modelButton" class="btn btn-primary model-button">Model</button>
+    <button type="button" onclick="window.location.href='{{ route('model.get') }}'" name="modelButton" class="btn btn-primary model-button">Change Phone</button>
 </form>
 
+<div class="android" {!! \App\Http\Helpers\PhoneModelHelper::getAndroid() !!}>
+    <div class="android-details"></div>
+    <div class="android-left"></div>
+    <div class="android-screen">
+        <div class="android-content">
+            <div class="container top-menu" style="position: absolute; z-index: 100; background-color: black; width: 100%; padding-left: 14em">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-wifi" viewBox="0 0 16 16">
+                    <path d="M15.384 6.115a.485.485 0 0 0-.047-.736A12.444 12.444 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.518.518 0 0 0 .668.05A11.448 11.448 0 0 1 8 4c2.507 0 4.827.802 6.716 2.164.205.148.49.13.668-.049z"/>
+                    <path d="M13.229 8.271a.482.482 0 0 0-.063-.745A9.455 9.455 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.576 1.336c.206.132.48.108.653-.065zm-2.183 2.183c.226-.226.185-.605-.1-.75A6.473 6.473 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.407.19.611.09A5.478 5.478 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.61-.091l.016-.015zM9.06 12.44c.196-.196.198-.52-.04-.66A1.99 1.99 0 0 0 8 11.5a1.99 1.99 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .707 0l.707-.707z"/>
+                </svg>
 
-<?php
-        $test =\App\Http\Helpers\PhoneModelHelper::getModel();
-        if ($test) {
-            echo '<div>hello</div>';
-//            \App\Http\Helpers\PhoneModelHelper::toggleModel();
-        } else {
-            echo '<div>goodbye</div>';
-//            \App\Http\Helpers\PhoneModelHelper::toggleModel();
-        }
-?>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-bar-chart" viewBox="0 0 16 16">
+                    <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"/>
+                </svg>
 
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-battery-half" viewBox="0 0 16 16">
+                    <path d="M2 6h5v4H2V6z"/>
+                    <path d="M2 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H2zm10 1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h10zm4 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z"/>
+                </svg>
 
-<div class="iphone">
+                <small><small><small><small>{{\Illuminate\Support\Carbon::now()->format('h:i')}}</small></small></small></small>
+            </div>
+            @yield('content')
+        </div>
+    </div>
+    <div class="android-home" onclick="location.href='/';" style="cursor: pointer"></div>
+</div>
+
+<div class="iphone" {!! \App\Http\Helpers\PhoneModelHelper::getIphone() !!}>
     <div class="iphone_power"></div>
     <div class="iphone_left"></div>
     <div class="iphone_details"></div>
