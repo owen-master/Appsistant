8340513c1ed4ed4981882a107263cd4231f0d717
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-10-07 11:50:28 +1000

    Change iphone top-bar colour.

--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -341,24 +341,24 @@
                 <div class="container top-menu" style="position: absolute; z-index: 100">
                     <div class="row">
                         <div class="col text-start">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor"
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="#000000"
                                  class="bi bi-bar-chart" viewBox="0 0 16 16">
                                 <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"/>
                             </svg>
 
-                            <small><small><small><small>Telstra</small></small></small></small>
+                            <small><small><small><small style="color: black;">Telstra</small></small></small></small>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-wifi-2 pb-1" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" class="bi bi-wifi-2 pb-1" viewBox="0 0 16 16">
                                 <path d="M13.229 8.271c.216-.216.194-.578-.063-.745A9.456 9.456 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.577 1.336c.205.132.48.108.652-.065zm-2.183 2.183c.226-.226.185-.605-.1-.75A6.473 6.473 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.408.19.611.09A5.478 5.478 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.611-.091l.015-.015zM9.06 12.44c.196-.196.198-.52-.04-.66A1.99 1.99 0 0 0 8 11.5a1.99 1.99 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .708 0l.707-.707z"/>
                             </svg>
                         </div>
                         <div class="col text-center">
-                            <small><small><small><small>{{\Illuminate\Support\Carbon::now()->format('h:i')}}</small></small></small></small>
+                            <small><small><small><small style="color: black;">{{\Illuminate\Support\Carbon::now()->format('h:i')}}</small></small></small></small>
                         </div>
-                        <div class="col text-end">
-                            <small><small><small><small>69%</small></small></small></small>
+                        <div class="col text-end ">
+                            <small><small><small><small style="color: black;">69%</small></small></small></small>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-battery-half" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="000000" class="bi bi-battery-half" viewBox="0 0 16 16">
                                 <path d="M2 6h5v4H2V6z"/>
                                 <path d="M2 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H2zm10 1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h10zm4 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z"/>
                             </svg>
